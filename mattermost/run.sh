33bf9bc74783eff86208fd356af9081457b132fe
Clayton Gulick <claytongulick@gmail.com>
2022-03-10 11:32:33 -0600

    added mattermost

---
+++ mattermost/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#make sure you have MM_CONFIG environment variable set to the location of your config file,
+#for example: export MM_CONFIG=/Users/claytongulick/source/telos/telos-v3/mattermost/config.json
+mattermost 
+
